f20e154ec2a6279d2d5e2ace5fc3375bd8fc6569
John Christopher Azcarraga <dyankristoper@anonymous-mini.local>
2023-05-06 01:35:21 +0800

    Feat: Create new TodoForm component

---
+++ src/components/TodoForm/TodoForm.svelte
@@ -0,0 +1,40 @@
+<script>
+    
+    import axios from 'axios';
+    import * as dotenv from 'dotenv';
+    dotenv.config();
+
+    let newTaskDescription = '';
+
+    /** 
+        @description    Send POST request to Firebase
+                        Adds a new task with Pending Status
+    */
+    const onSubmitHandler = ( e ) => {
+        e.preventDefault();
+
+        let newTask = {
+            name: newTaskDescription,
+            status: 'Pending'
+        };
+
+        axios.post( process.env.FIREBASE_URL , newTask )
+            .then( response => console.log( response ) )
+            .catch( e => console.error( e ))
+        // Clear new task value
+        newTask = '';
+    }
+
+</script>
+
+
+<form on:submit={ onSubmitHandler }>
+    <input
+        type ='text'
+        value={ newTaskDescription }
+    >
+    <input
+        type='submit'
+        value='+ Add Task'
+    >
+</form>
